ad9eb5c4219171db179bd9007ced6eb354cca4eb
Jonas Braun <jbraun@physik.hu-berlin.de>
2017-08-26 01:26:26 +0200

    fix user to admin@wepublic.me

--- wp_match/views.py
+++ wp_match/views.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import detail_route
 
 from django.http import JsonResponse
 
+from users.models import User
 
+
+@detail_route(methods=['get'], permission_classes=[IsAuthenticated])
 def index(request):
 
-    
+    user = User.objects.get(email='admin@wepublic.me')
 
-    return JsonResponse([ { 'content': 'asdf' } ], safe=False)
+    return JsonResponse([ { 'content': str(user) } ], safe=False)
