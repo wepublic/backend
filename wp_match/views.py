08237ccbb9b7012c27141c8ab861aca95a82e004
Celsa Diaz <celsadiazt@gmail.com>
2017-08-26 01:44:55 +0200

    add percentage calulation to views

--- wp_match/views.py
+++ wp_match/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import detail_route
+from wp_core.models import Answer
+from collections import Counter
 
 from django.http import JsonResponse
 
@@ -9,7 +11,13 @@ from users.models import User
 
 @detail_route(methods=['get'], permission_classes=[IsAuthenticated])
 def index(request):
-
     user = User.objects.get(email='admin@wepublic.me')
 
-    return JsonResponse([ { 'content': str(user) } ], safe=False)
+    answers = Answer.objects.filter(voteanswer__user=user, voteanswer__up=True)
+    c = Counter()
+    c.update([a.party for a in answers])
+    keys = [k.short_name for k in list(c.keys())]
+    a = dict(zip(keys, c.values()))
+    total = {(k, (v / sum(a.values())) * 100) for k, v in a.items()}
+
+    return JsonResponse([{'content': str(total)}], safe=False)
