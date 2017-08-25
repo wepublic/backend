e7f3e38c5fe96907bd8a8f74f9f061d69ed8c545
Jonas Braun <jbraun@physik.hu-berlin.de>
2017-08-25 18:14:33 +0200

    local settings

---
+++ wepublic_backend/settings_local.py
@@ -0,0 +1,35 @@
+import os
+
+
+SECRET_KEY = 'secretsecretsecret'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'database.sqlite')
+    }
+}
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_FILE_PATH = '/tmp/mails'
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+REPORT_MAILS = []
+REPORT_MAILS_ACTIVE = True
+
+SLACK_NOTIFICATIONS_ACTIVE = False
+SLACK_NOTIFICATIONS_URL = ''
