4ecb8d60abc902ddb75a0662701ef36b5c2d05f9
Marius Schulz <marius.schulz.mail@gmail.com>
2017-09-03 22:36:41 +0200

    Removed django_filters
    Isnt used and wasn't supposed to be committed

--- wepublic_backend/settings.py
+++ wepublic_backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'django_filters',
     'corsheaders',
     'pagedown',
     'rest_framework_swagger',
